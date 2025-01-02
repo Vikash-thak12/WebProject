dc9edc4c8dc4d7fac1bfa5e430e3469824dca847
Vikash Thakur <thakvikash622@gmail.com>
2025-01-02 23:54:43 +0530

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import Header from '@/components/Header'
 import React from 'react'
 
 const HomePage = () => {
   return (
     <div>
-      <Header />
       <h1>This is Home page</h1>
     </div>
   )
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,33 @@
+import Image from 'next/image'
+import Link from 'next/link';
 import React from 'react'
 
+const navItems = [
+    { id: 1, name: "Home", link: "/" },
+    { id: 2, name: "About", link: "/about" },
+    { id: 3, name: "Products", link: "/products" },
+    { id: 4, name: "Gallery", link: "/gallery" },
+    { id: 5, name: "Contact Us", link: "/contact-us" }
+];
+
+
 const Header = () => {
-  return (
-    <div>
-      Header Page
-    </div>
-  )
+    return (
+        <main className='md:py-5 md:px-10 py-4 px-2 flex items-center justify-between fixed top-0 w-full shadow-lg shadow-gray-200'>
+            <Image src={'/images/logo.png'} alt='Logo' width={400} height={100} className='hidden' />
+            <Image src={'/images/logo.png'} alt='Logo' width={200} height={100} />
+            <div className='md:flex items-center gap-5 hidden'>
+                {navItems.map((item) => (
+                    <Link key={item.id} href={item.link} className='relative group py-1'>
+                        <span className='font-bold text-xl'>{item.name}</span>
+                        <span className='absolute left-0 bottom-0 w-0 h-1 bg-red-500 group-hover:w-full transition-all'></span>
+                    </Link>
+
+                ))}
+            </div>
+
+        </main>
+    )
 }
 
 export default Header
