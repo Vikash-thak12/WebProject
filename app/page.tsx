3e1dbaa9895a60a5dd3e445ff370ae54bb70a439
Vikash Thakur <thakvikash622@gmail.com>
2025-01-03 00:07:59 +0530

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { WobbleCardDemo } from '@/components/WobbleCardDemo'
 import React from 'react'
 
 const HomePage = () => {
   return (
     <div>
       <h1>This is Home page</h1>
+      <WobbleCardDemo />
     </div>
   )
 }
