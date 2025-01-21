87579898fa30efe411df6080072db3675ecff404
Vikash Thakur <thakvikash622@gmail.com>
2025-01-21 22:36:43 +0530

    Created: static datas

---
+++ constants/index.ts
@@ -0,0 +1,42 @@
+export const slideData = [
+    {
+      title: "First One",
+      button: "Explore Component",
+      src: "/images/1.jpg",
+    },
+    {
+      title: "Urban Dreams",
+      button: "Explore Component",
+      src: "/images/2.jpg",
+    },
+    {
+      title: "Neon Nights",
+      button: "Explore Component",
+      src: "/images/3.jpg",
+    },
+    {
+      title: "Desert Whispers",
+      button: "Explore Component",
+      src: "/images/4.jpg",
+    },
+    {
+        title: "First One",
+        button: "Explore Component",
+        src: "/images/5.jpg",
+      },
+      {
+        title: "Urban Dreams",
+        button: "Explore Component",
+        src: "/images/6.jpg",
+      },
+      {
+        title: "Neon Nights",
+        button: "Explore Component",
+        src: "/images/7.jpg",
+      },
+      {
+        title: "Desert Whispers",
+        button: "Explore Component",
+        src: "/images/8.jpg",
+      },
+  ];
